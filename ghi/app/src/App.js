ef54a9d3fcb65605d9a2c4dcb969ef8d9b6535a5
Peter <petertrinh00@gmail.com>
2023-03-06 22:19:11 -0800

    revert merge

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -7,39 +7,25 @@ import AutoForm from './AutoForm';
 
 function App(props) {
 
-  import ManufacturerList from './ManufacturerList';
-  import ManufacturerForm from './ManufacturerForm';
-  import AutoList from './AutoList'
+  return (
+    <BrowserRouter>
+      <Nav />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="models">
+            <Route path="" element={<ModelList models={props.models} />} />
+          </Route>
+          <Route path="models">
+            <Route path="new" element={<ModelForm />} />
+          </Route>
+          <Route path="auto">
+            <Route path="new" element={<AutoForm />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
-
-  function App(props) {
-    return (
-      <BrowserRouter>
-        <Nav />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="models">
-              <Route path="" element={<ModelList models={props.models} />} />
-            </Route>
-            <Route path="models">
-              <Route path="new" element={<ModelForm />} />
-            </Route>
-            <Route path="auto">
-              <Route path="new" element={<AutoForm />} />
-              <Route path="manufacturers">
-                <Route path="" element={<ManufacturerList manufacturers={props.manufacturers} />} />
-              </Route>
-              <Route path="manufacturers">
-                <Route path="new" element={<ManufacturerForm />} />
-              </Route>
-              <Route path="automobiles">
-                <Route path="" element={<AutoList autos={props.autos} />} />
-              </Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
-    );
-  }
-
-  export default App;
+export default App;
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -9,38 +9,6 @@ root.render(
   </React.StrictMode>
 );
 
-
-async function loadManufaturers() {
-  const response = await fetch('http://localhost:8100/api/manufacturers/');
-  if (response.ok) {
-    const data = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App manufacturers={data.manufacturers} />
-      </React.StrictMode>
-    );
-  } else {
-    console.error(response);
-  }
-}
-loadManufaturers();
-
-
-async function loadAutos() {
-  const response = await fetch('http://localhost:8100/api/automobiles/');
-  if (response.ok) {
-    const data = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App autos={data.autos} />
-      </React.StrictMode>
-    );
-  } else {
-    console.error(response);
-  }
-}
-loadAutos();
-
 async function loadModels() {
   const response = await fetch('http://localhost:8100/api/models/');
   if (response.ok) {
@@ -55,39 +23,3 @@ async function loadModels() {
   }
 }
 loadModels();
-
-async function loadModels() {
-  const response = await fetch('http://localhost:8100/api/models/');
-
-  async function loadManufaturers() {
-    const response = await fetch('http://localhost:8100/api/manufacturers/');
-    if (response.ok) {
-      const data = await response.json();
-      root.render(
-        <React.StrictMode>
-          <App models={data.models} />
-          <App manufacturers={data.manufacturers} />
-        </React.StrictMode>
-      );
-    } else {
-      console.error(response);
-    }
-  }
-  loadModels();
-  loadManufaturers();
-
-
-  async function loadAutos() {
-    const response = await fetch('http://localhost:8100/api/automobiles/');
-    if (response.ok) {
-      const data = await response.json();
-      root.render(
-        <React.StrictMode>
-          <App autos={data.autos} />
-        </React.StrictMode>
-      );
-    } else {
-      console.error(response);
-    }
-  }
-  loadAutos();
